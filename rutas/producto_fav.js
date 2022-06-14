5b59ccf191f4d126166b670f45dfe1b7d40fa550
Jacqueline mancilla <80727358+jacmancilla@users.noreply.github.com>
2022-06-14 00:59:59 -0400

    ruta para prod fav por rut

--- rutas/producto_fav.js
+++ rutas/producto_fav.js
@@ -16,6 +16,19 @@ routes.post('/producto-favorito', (req, res) => {
         })
     })
 })
-
+//Obtener producto favorito por rut
+routes.get('/producto-favorito', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query('SELECT * FROM prod_favorito WHERE rut=? and cod_barra=?', [req.headers.rut, req.headers.cod_barra], (err, rows) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).json(rows)
+        })
+    })
+})
 
 module.exports = routes
