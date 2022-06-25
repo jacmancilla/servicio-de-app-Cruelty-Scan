e45dc55e23bebc899e7bc7840849ce9ad78f5b98
Jacqueline mancilla <80727358+jacmancilla@users.noreply.github.com>
2022-06-24 21:38:42 -0400

    Se agregan nuevas rutas para product favorito

--- rutas/producto_fav.js
+++ rutas/producto_fav.js
@@ -31,4 +31,44 @@ routes.get('/producto-favorito', (req, res) => {
     })
 })
 
+//Obtener producto favorito por rut
+routes.get('/producto-favorito/:rut', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query('SELECT * FROM prod_favorito WHERE rut=?', [req.params.rut], (err, rows) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).json(rows)
+        })
+    })
+})
+
+//Eliminar producto favorito por rut
+routes.delete('/producto-favorito', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query('SELECT * FROM prod_favorito WHERE rut=? and cod_barra=?', [req.body.rut, req.body.cod_barra], (err, rowsSelect) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            if (rowsSelect.length === 0) {
+                return res.status(500).send("Producto favorito no encontrado con el rut")
+            }
+            conn.query('DELETE FROM prod_favorito WHERE rut=? and cod_barra=?', [req.body.rut, req.body.cod_barra], (err, rows) => {
+                if (err) {
+                    return res.status(500).send(err)
+                }
+                res.status(200).send("Producto favorito eliminado")
+            })
+        })
+
+
+    })
+})
+
 module.exports = routes
